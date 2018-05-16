af2500f8122812ac711621eae34e59d2a8e1f834
claytonschroeder <clayton.schroeder@aulmni.ubc.ca>
2018-05-15 17:43:02 -0700

    add path

--- server/server.js
+++ server/server.js
@@ -1,15 +1,16 @@
 require('dotenv').config();
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const PORT = process.env.PORT || 8001;
-const server = express();
+const express     = require('express');
+const path        = require('path');
+const bodyParser  = require('body-parser');
+const PORT        = process.env.PORT || 8001;
+const server      = express();
 
 server.use(express.static('assets')); // This is actually a build directory in the parent
 server.use(bodyParser.json({limit: '500kb'})); // change the size limit
 
-server.get('/', function (request, response){
-  response.send('Hello');
+server.get('/', function (req, res){
+  res.sendFile(path.resolve(__dirname, '../', 'index.html'));
 });
 
 server.listen(PORT, () => {
